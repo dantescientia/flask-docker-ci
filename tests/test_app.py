77ac08afb7a229b5f4ef127775594de7bd01656a
dantescientia <zhuzbekdanik@gmail.com>
2025-09-06 14:35:34 +0200

    lint(test): format test_app.py for flake8

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,8 @@
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
 from flask_docker_ci.app import app
+
 
 def test_root():
     client = app.test_client()
     resp = client.get("/")
     assert resp.status_code == 200
     assert b"Hello" in resp.data
-
