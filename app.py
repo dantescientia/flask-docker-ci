2466eee04b1c2c8fde934e318ed9fc75949660ae
dantescientia <zhuzbekdanik@gmail.com>
2025-09-06 13:49:58 +0200

    fix: restore app.py with imports

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello, DevOps!"
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
