03ce80ee9ca166c67f39590cf81b37070b26b4a6
dantescientia <zhuzbekdanik@gmail.com>
2025-09-06 14:21:50 +0200

    test: fix import path for pytest; ci: set PYTHONPATH

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from flask_docker_ci.app import app
 
 def test_root():
@@ -5,3 +8,4 @@ def test_root():
     resp = client.get("/")
     assert resp.status_code == 200
     assert b"Hello" in resp.data
+
