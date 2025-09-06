c3edca4ce3657df140981a000466a7a7e25f1091
dantescientia <zhuzbekdanik@gmail.com>
2025-09-06 13:44:56 +0200

    fix: run on gunicorn with

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 FROM python:3.10-slim
 WORKDIR /app
 COPY requirements.txt .
-RUN pip install -r requirements.txt  
+RUN pip install -r requirements.txt
 COPY . .
-EXPOSE 5000
-CMD ["python3", "app.py"]
-
+CMD ["gunicorn", "-b", "0.0.0.0:8000", "app:app"]
 
--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask 
-
+import os
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello ():
-	return 'Hello DevOps!'
+@app.route("/")
+def hello():
+    return "Hello, DevOps!"
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
